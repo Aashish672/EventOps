90c0ffa7f0e5193f3ff7151d76b09716f4673102
Aashish672 <aashishsinghjsr81@gmail.com>
2026-09-25 12:07:18 +0530

    fix(api): fix import sorting in urls.py for CI

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,9 +4,8 @@ URL configuration for EventOps project.
 
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-
 from events.views import EventViewSet
+from rest_framework.routers import DefaultRouter
 from vendors.views import VendorViewSet
 
 # We instantiate a DefaultRouter. This magic DRF class automatically generates
